46b5888c106cce00f7f82c051df3fa6fdbc4e2e2
Evgenia <evgenya-7495@mail.ru>
2024-03-11 19:36:54 +0500

    third-try

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,16 +5,20 @@ function getHole( index ) {
     return document.getElementById('hole' + index);
 }
 
-for (let i = 1; i < 9; i++) {
-    elementHole = getHole( i );
+for (let i = 1; i < 10; i++) {
+  const elementHole = getHole(i);
     elementHole.addEventListener("click", function() {
       if ( elementHole.className.includes( 'hole_has-mole' ) ) {
         deadCounter.textContent = Number(deadCounter.textContent) + 1;
         if (Number(deadCounter.textContent) == 10) alert("Победа!");
+        clearStatistics();
+        startNewGame();
       }
       else {
         lostCounter.textContent = Number(lostCounter.textContent) + 1;
         if (Number(lostCounter.textContent) == 5) alert("Вы проиграли!");
+        clearStatistics();
+        startNewGame();
       }
     });
 }
